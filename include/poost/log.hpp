aa4e2a662b51ee7f8ac73a7f4463a78772064764
Roman Madyanov <romantaken@gmail.com>
2024-06-26 15:29:46 +0300

    Use fmt instad of std::format

--- include/poost/log.hpp
+++ include/poost/log.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cstdlib>
-#include <format>
+#include <fmt/format.h>
 #include <ostream>
 
 #define POOST_TRACE(...) POOST_TRACE_EX(poost::log::global, __VA_ARGS__)
@@ -67,18 +67,18 @@ const char *log_level_color(LogLevel level);
 
 template <typename... Args>
 void log_print(const LogSettings &settings, LogLevel level,
-               std::format_string<Args...> format, Args &&...args) {
+               fmt::format_string<Args...> format, Args &&...args) {
 
     const char *label = log_level_label(level);
 
     if (!settings.use_colors) {
-        *settings.stream << std::format("[{:<5}] ", label);
+        *settings.stream << fmt::format("[{:<5}] ", label);
     } else {
         const char *color = log_level_color(level);
-        *settings.stream << std::format("{}[{:<5}]\x1b[0m ", color, label);
+        *settings.stream << fmt::format("{}[{:<5}]\x1b[0m ", color, label);
     }
 
-    *settings.stream << std::format(format, std::forward<Args>(args)...)
+    *settings.stream << fmt::format(format, std::forward<Args>(args)...)
                      << "\n";
 }
 
